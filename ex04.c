6cfd90a3bda608c7749bd56694c3520796d954e9
Sandy Pirôpo <121200148+sandypiropo@users.noreply.github.com>
2023-03-14 23:28:39 -0300

    calcular idade

---
+++ ex04.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+
+int main() {
+    int dia, mes, ano, idade;
+    time_t agora = time(NULL); // Obter a data e hora atual
+    struct tm *dataAtual = localtime(&agora); // Converter para struct tm
+    
+    printf("Digite sua data de nascimento (DD MM AAAA): ");
+    scanf("%d %d %d", &dia, &mes, &ano);
+    
+    idade = dataAtual->tm_year + 1900 - ano; // Calcular a idade baseado no ano de nascimento
+    if (mes > dataAtual->tm_mon + 1 || (mes == dataAtual->tm_mon + 1 && dia > dataAtual->tm_mday)) {
+        idade--; // Subtrair 1 se o aniversário ainda não ocorreu este ano
+    }
+    
+    printf("Sua idade = %d anos.\n", idade);
+    
+    return 0;
+}
+
