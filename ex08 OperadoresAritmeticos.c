ab0b56dc1b0453e1534514bc22b18a4014907029
Sandy Pirôpo <121200148+sandypiropo@users.noreply.github.com>
2023-03-18 21:17:57 -0300

    up

--- ex08 OperadoresAritmeticos.c
+++ ex08 OperadoresAritmeticos.c
@@ -2,7 +2,7 @@
 
 int main(){
 	
-	int a, b, soma, sub, mul, div;
+	int a, b, soma, sub, mul, divi;
 	
 	printf("Digite o valor A: ");
 	scanf("%d", &a);
@@ -12,13 +12,13 @@ int main(){
 	soma = a + b;
 	sub = a - b;
 	mul = a * b;
-	div = a / b;
+	divi = a / b;
 	
 	printf("Resultados: \n");
 	printf("Soma: %d + %d = %d \n", a, b, soma);
 	printf("Subtração: %d - %d = %d \n", a, b, sub);
 	printf("Multiplicação: %d x %d = %d \n", a, b, mul);
-	printf("Divisão: %d / %d = %d \n", a, b, div);
+	printf("Divisão: %d / %d = %d \n", a, b, divi);
 }
 
 
