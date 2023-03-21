25287ab265a2e2ecfeb230c84e247b4c82907172
Sandy Pirôpo <121200148+sandypiropo@users.noreply.github.com>
2023-03-20 22:03:56 -0300

    update

---
+++ ex10 SwitchCase.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(){
+
+	int dia;
+	
+	printf("Insira um valor de 1 a 7: ");
+	scanf("%d", &dia);
+	
+	switch(dia){
+		case 1: 
+			printf("Domingo.\n");
+			break;
+		case 2:
+			printf("Segunda-feira.\n");
+			break;
+		case 3: 
+			printf("Terça-feira.\n");
+			break;
+		case 4:
+			printf("Quarta-feira.\n");
+			break;
+		case 5:
+			printf("Quinta-feira.\n");
+			break;
+		case 6:
+			printf("Sexta-feira.\n");
+			break;
+		case 7:
+			printf("Sabado.\n");
+			break;
+		default:
+			printf("Valor inválido.\n");
+			break;
+	}
+	
+}
+
+
