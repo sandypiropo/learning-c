1c6b54521c0de9bda4313abb856b70b7201455a7
Sandy Pirôpo <121200148+sandypiropo@users.noreply.github.com>
2023-03-24 22:25:04 -0300

    update

---
+++ ex12 DoWhile.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* comando do-while, o do define o bloco de comando e o while leva ; */
+
+int main(){
+	
+	int i=1;
+	
+	do{
+		printf("%d ", i);
+		i++;
+	}while(i <= 10);
+}
